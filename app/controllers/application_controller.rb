39f22a9b3abe7f191dc0432b104eb77f6d907948
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-01 13:52:27 +0530

    controllers: application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def current_user
+    header = request.headers['Authorization']
+    token = header.split(' ').last
+    decoded = JWT.decode(token, ENV['JWT_SECRET'])[0]
+    @current_user ||= User.find(decoded['user_id'])
+  rescue
+    head :unauthorized
+  end
 end
