c7e595ef9fbb1681476b1f9f878a4f43284a5747
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-03 16:02:12 +0530

    app: services[recommendation_service]

--- app/services/recommendation_service.rb
+++ app/services/recommendation_service.rb
@@ -6,8 +6,9 @@ class MlRecommendationService
 
   def self.get_recommendations(video_id, top_k = 5)
     response = get("/recommend", query: { video_id: video_id, top_k: top_k })
+    JSON.parse(response.body)['recommended_video_ids']
   rescue => e
-    Rails.logger.error("ML API Error: #{e}")
+    Rails.logger.error("ML API error: #{e}")
     []
   end
 end
